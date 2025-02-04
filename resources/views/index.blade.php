2a15d61364987cc4bae5668ae9a66d0703fe2d8c
OmarMidanii <omarmidani90@gmail.com>
2025-02-04 15:44:03 +0200

    V3.0

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="icon" sizes="100x100" type="image/png" href="../../public/assets/logo-QvWkdf2K.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ابو عبدو سحر الشرق</title>
+    <link rel="stylesheet" crossorigin href="../../public/assets/index-CtrVZXv8.css">
+</head>
+
+<body>
+    <div id="root"></div>
+    <script type="module" crossorigin src={{asset('assets/index-B6wPEGkN.js')}}></script>
+</body>
+
+</html>
