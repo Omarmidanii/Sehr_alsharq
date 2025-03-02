bda1dc9f703fb4e9af71629e36838bfdf947a055
OmarMidanii <omarmidani90@gmail.com>
2025-03-02 17:13:28 +0200

    Responsive Design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,15 +3,15 @@
 
 <head>
     <meta charset="UTF-8" />
-    <link rel="icon" sizes="100x100" type="image/png" href="../../public/assets/logo-QvWkdf2K.png" />
+    <link rel="icon" sizes="100x100" type="image/png" href={{asset('assets/logo-QvWkdf2K.png')}} />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>ابو عبدو سحر الشرق</title>
-    <link rel="stylesheet" crossorigin href="../../public/assets/index-CtrVZXv8.css">
+    <link rel="stylesheet" crossorigin href={{asset('assets/index-B_zA3d3V.css')}}>
 </head>
 
 <body>
     <div id="root"></div>
-    <script type="module" crossorigin src={{asset('assets/index-BRyNK8Vz.js')}}></script>
+    <script type="module" crossorigin src={{asset('assets/index-CULrA5mY.js')}}></script>
 </body>
 
 </html>
